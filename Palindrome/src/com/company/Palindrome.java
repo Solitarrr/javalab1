9c70ee7324d68c591818e5b0a8c43b7283a2aa43
Solitarrr <snill.ford19@gmail.com>
2022-02-11 11:33:30 +0300

    First commit for Palindrome

---
+++ Palindrome/src/com/company/Palindrome.java
@@ -0,0 +1,17 @@
+package com.company;
+//Класс, показывающий является ли строка палиндромом
+public class Palindrome {
+
+    public static void main(String[] args) {
+	for (int i=0; i<args.length; i++){
+        String s = args[i];
+        }
+    }
+    //Метод, позволяющий полностью изменить символы в строке
+    public static String reverseString(String s){
+        String a = "";
+        for (int i = s.length()-1; i >= 0; i--)
+            a += s.charAt(i);
+        return a;
+    }
+}
