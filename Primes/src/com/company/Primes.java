18b2251837fd3443720526237f2a761e7df3c38e
Solitarrr <snill.ford19@gmail.com>
2022-02-11 11:02:26 +0300

    First commit

---
+++ Primes/src/com/company/Primes.java
@@ -0,0 +1,17 @@
+package com.company;
+//Класс, выводящий все простые числа меньше 100
+public class Primes {
+
+    public static void main(String[] args) {
+        for (int i = 2; i < 100; i++) {
+
+        }
+    }
+    //Метод, проверяющий является ли число простым
+    public static boolean isPrime(int n){
+        for (int i = 2; i < n; i++)
+        if (n%i == 0)
+            return false;
+        return true;
+    }
+}
